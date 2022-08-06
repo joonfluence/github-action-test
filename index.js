96d214a36c90ff92a4fc77137eb9031e6595172d
joonho lee <goodfluence@naver.com>
2022-08-07 00:52:18 +0900

    logic change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function fibonacci(n) {
-  if (n < 0) {
-    throw new Error("Fibonacci is only defined for non-negative integers!");
-  }
+  // if (n < 0) {
+  //   throw new Error("Fibonacci is only defined for non-negative integers!");
+  // }
 
   if (n <= 1) {
     return n;
